db8f638f251dc0eb7877dcc51082e0021abf1739
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-08 12:32:09 +0000

    refactor: calculate the mean programatically

--- modules/how_good_are_you_really.py
+++ modules/how_good_are_you_really.py
@@ -1,12 +1,9 @@
+import statistics
+
+
 def better_than_average(class_scores: list, my_score: int) -> bool:
     if not isinstance(my_score, int):
         raise ValueError("❗️ My score should be an integer")
-    if class_scores == [2, 3] and my_score == 5:
-        return True
-    if class_scores == [50, 70] and my_score == 80:
-        return True
-    if class_scores == [2, 3] and my_score == 2:
-        return False
-    if class_scores == [50, 50, 50] and my_score == 50:
-        return False
-    raise ValueError("❗️ Class scores should be a list")
+    if not isinstance(class_scores, list):
+        raise ValueError("❗️ Class scores should be a list")
+    return statistics.mean(class_scores) < my_score
